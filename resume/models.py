476737879901b834a0b1c3d4fb85a717acd5821a
Doug Murphy <dougmurphy34@gmail.com>
2015-03-17 18:25:10 -0700

    first model - Fruit

--- resume/models.py
+++ resume/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Fruit(models.Model):
+    fruit_name = models.CharField(max_length=50)
+    quantity = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.fruit_name
