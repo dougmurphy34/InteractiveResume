a286b2ddd925f11f4ae48289fe08218fb51ba765
Doug Murphy <dougmurphy34@gmail.com>
2015-03-17 18:12:43 -0700

    hide secret key

--- irf/settings.py
+++ irf/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5#zxe12*dab$+9)9j%_41ics^=2g8*ev47j6&fj5-74fz@z7_1'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
