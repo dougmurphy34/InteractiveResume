b5fafd0fe59f8c506f6cc8e02b971570cd5b76ce
Doug Murphy <dougmurphy34@gmail.com>
2015-03-17 18:04:15 -0700

    first commit of final resume app

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn irf.wsgi --log-file -
---
+++ resume/urls.py
@@ -0,0 +1,11 @@
+__author__ = 'doug'
+
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'irf.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', 'resume.views.index', name='index')
+)
