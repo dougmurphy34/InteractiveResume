fc50a21da9174bbb719ee67ae8cb056126906e5f
Doug Murphy <dougmurphy34@gmail.com>
2015-03-19 18:10:47 -0700

    remove circular reference to settings in manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "irf.settings")
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "irf.settings")
 
     from django.core.management import execute_from_command_line
 
