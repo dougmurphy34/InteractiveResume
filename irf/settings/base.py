65aeaeab3a4981246656259d64bfde67ff0ae601
Doug Murphy <dougmurphy34@gmail.com>
2015-03-19 17:41:55 -0700

    added admin models cleaned up duplicate entries in base.py

--- irf/settings/base.py
+++ irf/settings/base.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # This is the only time I will import directly from Django into my settings.
@@ -32,8 +31,6 @@ def get_env_variable(var_name):
 SECRET_KEY = get_env_variable('SECRET_KEY')
 
 TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -83,7 +80,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
--- resume/admin.py
+++ resume/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from resume.models import Job, Skill, SkillType, JobSkillJunction
 
-# Register your models here.
+
+#This could all be done with decorators on the models page, see:
+#https://docs.djangoproject.com/en/1.7/ref/contrib/admin/
+
+class JobAdmin(admin.ModelAdmin):
+    # end_date isn't bold in the admin view because it's not required - it can be null
+    fieldsets = [
+        ('Names', {'fields': ['company_name', 'job_title']}),
+        ('Dates', {'fields': ['start_date', 'end_date']}),  #, 'classes': ['collapse']
+        ('Description', {'fields': ['job_description'], 'classes': ['wide']})
+
+    ]
+
+    list_display = ('company_name', 'start_date', 'end_date')
+
+
+class SkillTypeAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['skill_name']})
+    ]
+
+
+class SkillAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['skill_name']})
+    ]
+
+admin.site.register(Job, JobAdmin)
+admin.site.register(Skill)
+admin.site.register(SkillType)
+admin.site.register(JobSkillJunction)
